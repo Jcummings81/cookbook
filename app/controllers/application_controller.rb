d09ef7a384d33c56498f82812323cba050cb606b
Jesse Cummings <jesse.cummings.1981@gmail.com>
2018-10-01 12:10:30 -0600

    added addingredient and stubbed out recipe and addrecipe

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::API
+    def render_error(obj)
+        #want-- obj.errors.full_messages
+
+        render json: { errors: obj.errors.full_messages }, status: 422
+        
+    end
 end
---
+++ client/src/Recipe.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import axios from 'axios'
+import {
+    Form,
+    Card,
+    Button,
+    Header,
+} from 'semantic-ui-react'
+
+class Recipe extends React.Component {
+render () {
+    return (null)
+}
+}
+
+export default Recipe
